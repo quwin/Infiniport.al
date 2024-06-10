34b1129ec0a12523dad52a19aac174a0a5b8a66b
Ethan Tran <ethantran.et77@gmail.com>
2024-06-10 19:46:05 +0000

    corrected '>=' init_job_views -> '<'

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ async def init_job_views(client: discord.Client):
   for row in jobs:
       print(row)
       expiration_date = float(row[1])
-      if current_time >= expiration_date:
+      if current_time < expiration_date:
         view_lifetime = expiration_date - current_time
         try:
           message_id = row[2]
