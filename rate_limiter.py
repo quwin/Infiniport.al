4cc45330d156c2e4fb078992824e606408f15392
Ethan Tran <ethantran.et77@gmail.com>
2024-06-11 14:41:09 +0000

    optimized AdaptiveRateLimiter

--- rate_limiter.py
+++ rate_limiter.py
@@ -6,24 +6,23 @@ class AdaptiveRateLimiter:
         self.calls = calls
         self.per_second = per_second
         self.semaphore = asyncio.Semaphore(calls)
-        self.times = asyncio.Queue(maxsize=calls)
+        self.times = []
 
     async def __aenter__(self):
         await self.semaphore.acquire()
         current_time = time.time()
-        if self.times.qsize() == self.calls:
-            oldest_time = await self.times.get()
+        if len(self.times) == self.calls:
+            oldest_time = self.times.pop(0)
             time_to_wait = oldest_time + self.per_second - current_time
             if time_to_wait > 0:
-#                print(f'Waiting for {time_to_wait:.2f} seconds at limiter')
                 await asyncio.sleep(time_to_wait)
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
         current_time = time.time()
-        await self.times.put(current_time)
+        self.times.append(current_time)
         self.semaphore.release()
 
     def reset(self):
-        self.times = asyncio.Queue(maxsize=self.calls)
+        self.times.clear()
         self.semaphore = asyncio.Semaphore(self.calls)
