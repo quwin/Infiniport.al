e87cea742d549a674d46e0ad94d1260c55ca91dc
Ethan Tran <ethantran.et77@gmail.com>
2024-07-07 18:43:08 +0000

    made server_id optional,

--- webserver.py
+++ webserver.py
@@ -11,12 +11,18 @@ app = Flask(__name__)
 
 DATABASE = 'leaderboard.db'
 
+
 def get_db():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/leaderboard/<table_name>/<order>/<page_number>/<server_id>', methods=['GET'])
+
+@app.route('/leaderboard/<table_name>/<order>/<page_number>/',
+           defaults={'server_id': None},
+           methods=['GET'])
+@app.route('/leaderboard/<table_name>/<order>/<page_number>/<server_id>',
+           methods=['GET'])
 def get_leaderboard(table_name, order, page_number, server_id):
     valid_orders = ['level', 'exp']
     valid_tables = SKILLS.copy()
@@ -30,19 +36,20 @@ def get_leaderboard(table_name, order, page_number, server_id):
     try:
         db = get_db()
         if server_id:
-            server_id = int(server_id)
-            cursor = db.execute(f'''
+            cursor = db.execute(
+                f'''
                 SELECT u.username, u.{order}
                 FROM {table_name} u
                 JOIN guild_{server_id} gm ON gm.user_id = u.user_id
                 ORDER BY u.{order} DESC
-                LIMIT 10 OFFSET ?''', (offset,))
+                LIMIT 10 OFFSET ?''', (offset, ))
         else:
-            cursor = db.execute(f'''
+            cursor = db.execute(
+                f'''
                 SELECT username, {order}
                 FROM {table_name}
                 ORDER BY {order} DESC
-                LIMIT 10 OFFSET ?''', (offset,))
+                LIMIT 10 OFFSET ?''', (offset, ))
 
         rows = cursor.fetchall()
         return jsonify([dict(row) for row in rows])
@@ -50,6 +57,7 @@ def get_leaderboard(table_name, order, page_number, server_id):
         return jsonify({'error': str(e)}), 500
     finally:
         db.close()
+
 
 async def get_access_token(auth_code):
     token_url = "https://api.collab.land/oauth2/token"
@@ -60,9 +68,11 @@ async def get_access_token(auth_code):
         "client_secret": COLLAB_SECRET,
         "redirect_uri": REDIRECT_URI,
     }
-    async with aiohttp.ClientSession() as session, session.post(token_url, data=data) as response:
+    async with aiohttp.ClientSession() as session, session.post(
+            token_url, data=data) as response:
         return await response.json()
-        
+
+
 # Request linked wallet(s) from Collab.land API
 async def get_user_wallets(access_token, limit=None, pagination_token=None):
     wallets_url = "https://api.collab.land/account/wallets"
@@ -78,13 +88,16 @@ async def get_user_wallets(access_token, limit=None, pagination_token=None):
         'Authorization': f"Bearer {access_token}"
     }
 
-    async with aiohttp.ClientSession() as session, session.get(wallets_url, headers=headers, params=params) as response:
+    async with aiohttp.ClientSession() as session, session.get(
+            wallets_url, headers=headers, params=params) as response:
         return await response.json()
+
 
 # Async wrapper for looking for a given profile from their address from the Pixels.xyz API
 async def look_for_profile(wallet_address):
     async with aiohttp.ClientSession() as session:
         return await profile_finder(session, wallet_address)
+
 
 @app.route("/oauth2/callback")
 def oauth2_callback():
@@ -93,40 +106,41 @@ def oauth2_callback():
     if auth_code and user_id:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        access_token_data = loop.run_until_complete(get_access_token(auth_code))
-        
+        access_token_data = loop.run_until_complete(
+            get_access_token(auth_code))
+
         access_token = access_token_data.get("access_token")
         refresh_token = access_token_data.get("refresh_token")
-        loop.run_until_complete(add_collab_tokens(user_id, access_token, refresh_token))
-        
-        user_wallets_data = loop.run_until_complete(get_user_wallets(access_token))
+        loop.run_until_complete(
+            add_collab_tokens(user_id, access_token, refresh_token))
+
+        user_wallets_data = loop.run_until_complete(
+            get_user_wallets(access_token))
         addresses = []
         player_ids = []
-        
+
         for wallet in user_wallets_data.get("items", []):
             type = wallet.get("walletType")
             if type == 'evm' or type == 'metamask' or type == 'ronin':
                 address = wallet.get("address")
-                account_data = loop.run_until_complete(look_for_profile(address))
+                account_data = loop.run_until_complete(
+                    look_for_profile(address))
                 if address and account_data:
                     addresses.append(address)
                     player_ids.append(account_data.get("_id"))
-                    
+
         if addresses and player_ids:
             formatted_addresses = " ".join(addresses)
             formatted_player_ids = " ".join(player_ids)
-            loop.run_until_complete(add_collab_wallets(
-                user_id, 
-                formatted_addresses,
-                formatted_player_ids
-                )
-            )
-            
-            
+            loop.run_until_complete(
+                add_collab_wallets(user_id, formatted_addresses,
+                                   formatted_player_ids))
+
         print(f"User ID: \n{user_id}, \n User Wallets: \n{user_wallets_data}")
         return redirect(url_for('success'))
     else:
         return redirect(url_for('error'))
+
 
 @app.route("/success")
 def success():
@@ -147,17 +161,21 @@ def success():
         </html>
     """)
 
+
 @app.route("/error")
 def error():
     return "<h1>Error: Authorization code not found.</h1>"
+
 
 @app.route('/')
 def serve_index():
     return send_from_directory('public', 'index.html')
 
+
 @app.route('/<path:path>')
 def serve_static(path):
     return send_from_directory('public', path)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
